e4edcc1cf0add1fec2825c40f33c29758b2a05f5
Sofia <sofiaantenor1418@gmail.com>
2024-06-14 16:58:18 -0300

    Fazendo ligações para cada candidato selecionado

--- Controle_Candidatos/src/candidatura/ProcessoSeletivo.java
+++ Controle_Candidatos/src/candidatura/ProcessoSeletivo.java
@@ -1,16 +1,46 @@
 package candidatura;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) throws Exception {
         System.out.println("Processo Seletivo");
         selecaoCandidatos(); // Chamada para selecionar candidatos
+        String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO"};
+        for(String candidato: candidatos){
+            entrandoEmContato(candidato);
+        }
         /* 
         analisarCandidato(1900.0);
         analisarCandidato(2200.0);
         analisarCandidato(2000.0);
         */
     }
+    static void entrandoEmContato(String candidato){
+        int tentativasRealizadas = 1;
+		boolean continuarTentando = true;
+		boolean atendeu=false;
+		do {
+			atendeu= atender();
+			continuarTentando = !atendeu;
+			if(continuarTentando)
+				tentativasRealizadas++;
+			else
+				System.out.println("CONTATO REALIZADO COM SUCESSO");
+			
+		}while(continuarTentando && tentativasRealizadas<3);
+		
+		if(atendeu)
+			System.out.println("CONSEGUIMOS CONTATO COM " + candidato +" NA " + tentativasRealizadas + " TENTATIVA");
+		else
+			System.out.println("NÃO CONSEGUIMOS CONTATO COM " + candidato +", NÚMERO MAXIMO TENTATIVAS " + tentativasRealizadas + " REALIZADA");
+		
+    }
+    //método auxiliar
+	static boolean atender() {
+		return new Random().nextInt(3)==1;	
+	}
+
     static void imprimirSelecionados(){
         String [] candidatos = {"FELIPE","MÁRCIA","JULIA","PAULO","AUGUSTO"};
         System.out.println("Imprimindo a lista de candidatos informando o indice do elemento");
